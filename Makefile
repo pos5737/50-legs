654ffc6724f137c8e03aff8d09fcfd3fb8598200
Carlisle Rainey <carlislerainey@gmail.com>
2018-07-29 15:57:21 -0400

    Add script to compute SDs by party-state-year

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+all: data/sds.rds data/data-sources.md
 
 # clean the raw data; this builds .rds and .csv versions
 ## note that $< :: the filename of the first prereq
@@ -8,7 +9,16 @@ data/shor-mccarty.rds: R/clean-data.R data/raw/shor_mccarty_1993-2016_individual
 data/data-sources.md: data/data-sources.Rmd data/shor-mccarty.rds 
 	Rscript -e 'rmarkdown::render("$<")'
 
+# create data frame of SDs by party-state-year
+data/sds.rds: R/calc-sd.R data/shor-mccarty.rds
+	Rscript $<
+
+# create data frame of SDs by party-state-year
+doc/tabs/sd-republicans.tex: R/calc-sd.R data/shor-mccarty.rds
+	Rscript $<
+
 # clean eliminates all compiled files for a fresh start	
 clean:
 	rm -f data/data-sources.md 
 	rm -f data/shor-mccarty.csv data/shor-mccarty.rds
+	rm -f data/sds.csv data/sds.rds doc/tabs/republicans-sd.tex
---
+++ R/calc-sd.R
@@ -0,0 +1,30 @@
+
+# load packages
+library(tidyverse)
+library(kableExtra)
+library(formattable)
+
+# load data
+leg_df <- read_rds("data/shor-mccarty.rds") %>%
+  filter(chamber == "house") %>%  # keep only house
+  filter(party != "X") %>%  # keep only Ds and Rs
+  glimpse()  # quick look
+
+# find standard deviation for each party in each party-state-year
+sum_df <- leg_df %>%
+  group_by(state, party, year) %>%  # set groups at state-party-year
+  summarize(sd = sd(ideology, na.rm = TRUE)) %>%  # calculate sd by group
+  write_csv("data/sds.csv") %>%  # write to .csv file (for github)
+  write_rds("data/sds.rds") %>%  # write to .rds file (for analysis)
+  glimpse()  # quick look
+
+# create a table for Republicans
+options(knitr.kable.NA = "")  # show NAs as empty
+filter(sum_df, party == "R") %>%  # keep only Rs 
+  ungroup() %>%  # unset groups
+  select(-party) %>%  # drop party variable
+  rename(State = state) %>%  # rename state variable for table
+  spread(year, sd) %>%  # put years as columns
+  kable(format = "latex", booktabs = TRUE, digits = 2) %>%  # create table 
+  kable_styling(latex_options = c("striped", "scale_down")) %>%  # style table
+  cat(file = "doc/tabs/republicans-sd.tex")  # write table to file
